7910a3097cf6f1a4099d8f2ac220d133d189438d
mvdriest <dutchmenvsgaming@gmail.com>
2026-03-19 12:42:58 +0100

    Update AboutMe component: Revamp layout and content to enhance storytelling and visual appeal.

--- app/components/AboutMe.vue
+++ app/components/AboutMe.vue
@@ -1,10 +1,19 @@
 <template>
-    <section class="relative flex min-h-screen flex-col items-center justify-center gap-12 px-4 py-16 text-center">
-        <h1 class="text-5xl font-bold tracking-tight text-gray-200 sm:text-[5rem]">
-        Over mij
-        </h1>
-        <p class="max-w-xl text-lg text-gray-400">
-        Ik ben een gepassioneerde webontwikkelaar met een liefde voor het creëren van prachtige, functionele websites. Met meer dan 10 jaar ervaring in de industrie, heb ik een breed scala aan projecten gerealiseerd, van kleine bedrijfswebsites tot grote e-commerce platforms. Mijn expertise ligt in het bouwen van snelle, responsieve en gebruiksvriendelijke websites die niet alleen goed presteren, maar ook een blijvende indruk achterlaten.
-        </p>
+    <section class="relative flex flex-col items-center justify-center gap-12 px-4 py-24 bg-gray-200">
+        <LayoutTheContainer>
+            <h1 class="text-7xl uppercase font-bold max-w-3xl leading-tight text-left">
+            Alles begint bij het verhaal.
+            </h1>
+            <div class="grid md:grid-cols-2 gap-12 mt-12">
+                <nuxt-img class="max-h-[28rem] object-cover w-full rounded-2xl" src="/images/other/camerafotoik.jpg" alt="" format="webp" />
+                <div class="flex flex-col gap-7">
+                    <h2 class="text-3xl font-semibold text-left max-w-2xl font-family-helvetica -tracking-[1px] text-dark-600">Over de afgelopen jaren heb ik gewerkt aan een breed scala aan merken: van branding en webdesign tot fotografie, video en campagnes.</h2>
+                    <p class="text-xl font-semibold text-left max-w-2xl font-family-helvetica -tracking-[1px] text-dark-600">Die brede ervaring zorgt ervoor dat ik altijd zie hoe alles samenhangt.  Ik geloof dat een merk niet alleen mooi moet zijn, maar moet raken, onthouden worden én resultaat moet opleveren.</p>
+                    <p class="text-xl font-semibold text-left max-w-2xl font-family-helvetica -tracking-[1px] opacity-70 text-dark-600">Daarom maak ik geen losse plaatjes of sites.
+
+                    Ik bouw complete ervaringen: video die emotie overbrengt, foto’s die verhalen vertellen, websites die converteren, branding die consistent blijft hangen.</p>
+                </div>
+            </div>
+        </LayoutTheContainer>
     </section>
 </template>
